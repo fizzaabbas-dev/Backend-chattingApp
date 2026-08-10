0263287e34be2a64463a48f1081d9b3d63c80784
fizzaabbas-dev <fizzaabbas.398@gmail.com>
2026-08-10 10:27:39 -0700

    backend

---
+++ index.js
@@ -0,0 +1,36 @@
+import cors from 'cors';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+app.get("/", (req,res)=>{
+    res.send("<h1> Hello from Realtime Socket Chat Server </h1>");
+})
+io.on("connection",(socket)=>
+{
+    console.log("a user connected", socket.id);
+    socket.on("join" , (roomID)=>{
+        socket.join(roomID);
+    });
+    socket.on("leave" , (roomID)=>{
+        socket.leave(roomID);
+    });
+    socket.on("send", (message)=>{
+        console.log(message)
+        socket.to(message.room).emit("message",message);
+    });
+});
+
+server.listen(5050, () => {
+  console.log("Server is running on port 5050");
+});
