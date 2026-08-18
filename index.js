c237cc24363f13e6891c5c6974cc16e6b04c9a9a
fizzaabbas-dev <fizzaabbas.398@gmail.com>
2026-08-18 09:09:14 -0700

    changed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: " http://localhost:5173/",
+    origin: " http://chatting-app-frontend-one.vercel.app",
     methods: ["GET", "POST"],
     credentials: true
   }
