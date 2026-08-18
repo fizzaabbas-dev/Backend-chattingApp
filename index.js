748c31b764fb4383af2af5e9f82c5a629e85bf8e
fizzaabbas-dev <fizzaabbas.398@gmail.com>
2026-08-18 07:37:06 -0700

    changed

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5050, () => {
-  console.log("listening on *:5050");
+const PORT = process.env.PORT || 5050;
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
